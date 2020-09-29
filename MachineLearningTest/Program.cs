b705115260afc9d77e62ff9961c256405909635e
Rafał Klinowski <klinowskirafal@gmail.com>
2020-09-29 22:21:40 +0200

    Add project files.

---
+++ MachineLearningTest/Program.cs
@@ -0,0 +1,19 @@
+﻿using MachineLearningTestML.Model;
+using System;
+
+namespace myMLApp
+{
+	internal class Program
+	{
+		private static void Main()
+		{
+			// Add input data
+			var input = new ModelInput();
+			input.SentimentText = "This is my IP, fucker";
+
+			// Load model and predict output of sample data
+			ModelOutput result = ConsumeModel.Predict(input);
+			Console.WriteLine($"Text: {input.SentimentText}\nIs Toxic: {result.Prediction}");
+		}
+	}
+}
